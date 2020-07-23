2ea26fc7fc88e4e27afe693c3c6fe6fbea034762
jorge.vargas <jorge.vargas@homeadvisor.com>
2020-07-23 14:12:26 -0500

    simple rest endpoint with hystrix fallback and timeout

--- src/main/java/components/HistrixApp.java
+++ src/main/java/components/HistrixApp.java
@@ -5,18 +5,17 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @SpringBootApplication
 @RestController
 
 @EnableHystrix
-
-@EnableHystrixDashboard
 public class HistrixApp {
     public static void main(String[] args) {
         SpringApplication.run(HistrixApp.class, args);
@@ -24,10 +23,18 @@ public class HistrixApp {
 
     @RequestMapping(value = "/echo/{param}", method = RequestMethod.GET)
     @HystrixCommand(fallbackMethod = "fallback_hello", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
     })
     public String echo (@PathVariable(name = "param") String param ){
-        return "Echo... "+new HystrixCommandImpl(param).execute();
+        Random random=new Random();
+        int intRandom= random.nextInt(999);
+        System.out.println("IntRandom: "+intRandom);
+        try {
+            Thread.sleep(intRandom);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Echo... "+intRandom;
     }
 
     public String fallback_hello( String param  ){
